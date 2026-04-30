08c158cc2d2e0c4ef4a09d21f5e82a432c4c2b96
phakphoum38-stack <phakphoum38@gmail.com>
2026-04-30 11:18:55 +0700

    Update ProgressBar.jsx

--- frontend/components/ProgressBar.jsx
+++ frontend/components/ProgressBar.jsx
@@ -1,7 +1,19 @@
-export default function ProgressBar({ value }) {
+export default function ProgressBar({ data }) {
   return (
-    <div style={{ width: "100%", background: "#333" }}>
-      <div style={{ width: value + "%", background: "green", height: "10px" }} />
+    <div>
+      <p>Progress: {data.progress || 0}%</p>
+      <p>Speed: {data.speed_mb_s || 0} MB/s</p>
+      <p>ETA: {data.eta_sec || 0}s</p>
+
+      <div style={{ width: "100%", background: "#222", height: 10 }}>
+        <div
+          style={{
+            width: (data.progress || 0) + "%",
+            background: "green",
+            height: "100%"
+          }}
+        />
+      </div>
     </div>
   );
 }
