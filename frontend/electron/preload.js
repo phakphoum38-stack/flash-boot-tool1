85407f7ea1605eab7de1e8b66677c45d34ce7811
phakphoum38-stack <phakphoum38@gmail.com>
2026-05-03 23:20:27 +0700

    Update preload.js

--- frontend/electron/preload.js
+++ frontend/electron/preload.js
@@ -5,3 +5,11 @@ contextBridge.exposeInMainWorld("api", {
   onReady: (cb) => ipcRenderer.on("backend-ready", cb),
   onError: (cb) => ipcRenderer.on("backend-error", cb)
 });
+
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("api", {
+  onUpdateStatus: (cb) => ipcRenderer.on("update-status", (_, msg) => cb(msg)),
+  onUpdateProgress: (cb) => ipcRenderer.on("update-progress", (_, data) => cb(data)),
+  selectISO: () => ipcRenderer.invoke("select-iso")
+});
