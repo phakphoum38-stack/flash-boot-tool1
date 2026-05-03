936ace6210861526338e7d1b113e7a89cc8b528e
phakphoum38-stack <phakphoum38@gmail.com>
2026-05-04 01:00:54 +0700

    Update SpeedGraph.jsx

--- frontend/src/dashboard/SpeedGraph.jsx
+++ frontend/src/dashboard/SpeedGraph.jsx
@@ -1,1 +1,23 @@
+export default function SpeedGraph({ data }) {
 
+  return (
+    <div style={{ marginTop: 20 }}>
+
+      <h4>⚡ Speed</h4>
+
+      <div style={{
+        height: 10,
+        background: "#222"
+      }}>
+        <div style={{
+          width: (data.progress || 0) + "%",
+          height: "100%",
+          background: "lime"
+        }} />
+      </div>
+
+      <p>{data.speed || 0} MB/s</p>
+
+    </div>
+  );
+}
