f12b5cc79e145bacd37838c00ac2fcbf5bdb49e9
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-05 22:04:04 +0700

    Update preload.js

--- preload/preload.js
+++ preload/preload.js
@@ -1,1 +1,23 @@
+const { contextBridge, ipcRenderer } = require('electron')
 
+contextBridge.exposeInMainWorld('api', {
+
+  flashStart: (data) =>
+    ipcRenderer.invoke('flash:start', data),
+
+  cancelFlash: () =>
+    ipcRenderer.invoke('flash:cancel'),
+
+  getUsb: () =>
+    ipcRenderer.invoke('usb:list'),
+
+  smartUsb: (dev) =>
+    ipcRenderer.invoke('usb:smart', dev),
+
+  vmTest: (iso) =>
+    ipcRenderer.invoke('vm:test', iso),
+
+  onFlash: (cb) => {
+    ipcRenderer.on('flash:event', (_, d) => cb(d))
+  }
+})
