c4d403711e27a49ae407c09e2b58521b144e7170
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-05 22:10:18 +0700

    Update usb-service.js

--- electron/usb-service.js
+++ electron/usb-service.js
@@ -1,1 +1,19 @@
+ipcMain.handle("get-usb-devices", async () => {
 
+  const { stdout } = await execPromise(`
+powershell -NoProfile "
+Get-CimInstance Win32_DiskDrive |
+Where-Object {$_.InterfaceType -eq 'USB'} |
+Select DeviceID, Model, Size |
+ConvertTo-Json
+"
+  `)
+
+  const data = JSON.parse(stdout || "[]")
+
+  return (Array.isArray(data) ? data : [data]).map(d => ({
+    path: d.DeviceID,
+    name: d.Model,
+    size: Number(d.Size || 0)
+  }))
+})
