4509343a33e7cf98053c07dd36ace4ab8b788a73
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-05 22:03:25 +0700

    Update flash-manager.js

--- electron/flash-manager.js
+++ electron/flash-manager.js
@@ -1,1 +1,66 @@
+const { spawn } = require('child_process')
+let proc = null
 
+let lastEmit = 0
+
+function start(win, { mode, iso, device }) {
+
+  return new Promise((resolve) => {
+
+    proc = spawn('backend.exe', [mode, iso, device])
+
+    proc.stdout.on('data', (data) => {
+
+      const lines = data.toString().split('\n')
+
+      for (const line of lines) {
+
+        if (!line) continue
+
+        // throttle 60fps
+        const now = Date.now()
+        if (now - lastEmit < 16) continue
+        lastEmit = now
+
+        if (line.startsWith('PROGRESS:')) {
+          const v = +line.split(':')[1]
+
+          win.webContents.send('flash:event', {
+            type: 'progress',
+            value: v
+          })
+        }
+
+        if (line.startsWith('SPEED:')) {
+          win.webContents.send('flash:event', {
+            type: 'speed',
+            value: +line.split(':')[1]
+          })
+        }
+
+        if (line.startsWith('LOG:')) {
+          win.webContents.send('flash:event', {
+            type: 'log',
+            msg: line.replace('LOG:', '')
+          })
+        }
+      }
+    })
+
+    proc.on('close', (code) => {
+      win.webContents.send('flash:event', {
+        type: 'result',
+        success: code === 0
+      })
+
+      resolve({ success: code === 0 })
+    })
+  })
+}
+
+function cancel() {
+  if (proc) proc.kill()
+  proc = null
+}
+
+module.exports = { start, cancel }
